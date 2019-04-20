271fc23d67e2e581bf1f133d0b968da3a1ea9a8a
Shoeb Ahmed Tanjim <39959228+s-a-tanjim@users.noreply.github.com>
2019-04-20 19:43:52 +0600

    Update and rename Bluetooth_text.ino to Bluetooth_test.ino

--- Bluetooth_test.ino
+++ Bluetooth_test.ino
@@ -1,10 +1,9 @@
 void setup() {
 Serial.begin(9600);
- pinMode(7, OUTPUT); // put your setup code here, to run once:
+ pinMode(7, OUTPUT);
  }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
  if(Serial.available()>0)
    {     
@@ -13,9 +12,9 @@ void loop() {
       char data= Serial.read(); // reading the data received from the bluetooth module
       if(data=='F'){
         digitalWrite(7, HIGH);
-      }// when a is pressed on the app on your smart phone
+      }
       else if(data=='B'){
-        digitalWrite(7, LOW); // when d is pressed on the app on your smart phone
+        digitalWrite(7, LOW);
       }
       Serial.println(data);
    }
